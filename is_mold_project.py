e19f85bb0c9d4b6fc378faca417ad2f5a7411431
Tony Galmiche <tony.galmiche@infosaone.com>
2017-04-29 17:38:57 +0200

    add commentaire

--- is_mold_project.py
+++ is_mold_project.py
@@ -18,10 +18,8 @@ class is_mold_project(models.Model):
 
     name           = fields.Char("Nom du projet",size=40,required=True, select=True)
     client_id      = fields.Many2one('res.partner', 'Client')
-
-
-    #chef_projet_id = fields.Many2one('res.users', 'Chef de projet', domain = _get_group_chef_de_projet)
     chef_projet_id = fields.Many2one('res.users', 'Chef de projet')
+    commentaire    = fields.Char("Commentaire")
 
 
     mold_ids       = fields.One2many('is.mold', 'project', u"Moules")
